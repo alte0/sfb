0a987cb2d5da7c4a2c6500d0771e03fc1493de8f
Максим <makc-dmv@yandex.ru>
2020-09-30 10:06:29 +0500

    основной функционал

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+// Main js file
+// see more: https://github.com/vedees/webpack-template/blob/master/README.md#import-js-files
+import svg4everybody from 'svg4everybody';
+
+svg4everybody();
+console.info('console.info')
